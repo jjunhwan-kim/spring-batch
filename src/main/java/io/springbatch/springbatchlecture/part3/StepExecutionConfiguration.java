e2113d07d825362ca1fd940ebb3d09e131abbb06
JunHwan Kim <nefertiri972@gmail.com>
2022-10-17 23:27:02 +0900

    Add step execution configuration
    - Step Execution 객체는 Step에 대한 한 번의 시도를 의미하는 객체로 Step이 실행될 때 마다 생성
    - Step 중 하나라도 실패하면 JobExecution은 실패함
    - Job이 재시작 하더라도 이미 완료된 Step은 재실행되지 않고 실패한 Step만 실행됨
    - Step은 순서대로 실행되고 Step이 실패되면 다음 Step은 실행되지 않으므로 StepExecution 객체가 생성되지 않음. 즉 실제로 Step이 시작됬을때만 StepExecution을 생성함

--- src/main/java/io/springbatch/springbatchlecture/part3/StepExecutionConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/part3/StepExecutionConfiguration.java
@@ -7,10 +7,11 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
-//@Configuration
-public class StepConfiguration {
+@Configuration
+public class StepExecutionConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -20,6 +21,7 @@ public class StepConfiguration {
         return jobBuilderFactory.get("job")
                 .start(step1())
                 .next(step2())
+                .next(step3())
                 .build();
     }
 
@@ -38,9 +40,19 @@ public class StepConfiguration {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
                     System.out.println("step2 has executed");
+                    //throw new RuntimeException("step2 has failed");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
     }
 
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step3 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
 }
