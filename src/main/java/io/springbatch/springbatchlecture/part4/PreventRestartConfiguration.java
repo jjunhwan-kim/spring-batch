9fe48b9e6225e903003af21acc52a8cbb7331b88
JunHwan Kim <nefertiri972@gmail.com>
2022-10-26 22:16:19 +0900

    Add prevent restart configuration
    - Job의 재시작 여부를 설정
    - 기본 값은 true이고 false로 설정시 재시작을 지원하지 않음
    - Job이 실패한 이후 재시작하였을 때 JobRestartException 발생
    - Job의 실행이 처음이 아닌 경우 Job의 preventRestart 설정이 되어 있을 경우 JobRestartException 발생
    - SimpleJobLauncher.run 메서드에서 lastExecution이 null이 아닐 때, job.isRestartable 확인

--- src/main/java/io/springbatch/springbatchlecture/part4/PreventRestartConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/part4/PreventRestartConfiguration.java
@@ -5,13 +5,13 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
-//@Configuration
-public class ValidatorConfiguration {
+@Configuration
+public class PreventRestartConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -21,9 +21,7 @@ public class ValidatorConfiguration {
         return jobBuilderFactory.get("batchJob")
                 .start(step1())
                 .next(step2())
-                .next(step3())
-                //.validator(new CustomJobParametersValidator())
-                .validator(new DefaultJobParametersValidator(new String[]{"name", "date"}, new String[]{"count"}))
+                .preventRestart()
                 .build();
     }
 
@@ -41,18 +39,9 @@ public class ValidatorConfiguration {
     public Step step2() {
         return stepBuilderFactory.get("step2")
                 .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step2 has executed");
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
-    }
-
-    @Bean
-    public Step step3() {
-        return stepBuilderFactory.get("step3")
-                .tasklet((contribution, chunkContext) -> {
-                    System.out.println("step3 has executed");
-                    return RepeatStatus.FINISHED;
+                    throw new RuntimeException("step2 was failed");
+                    //System.out.println("step2 has executed");
+                    //return RepeatStatus.FINISHED;
                 })
                 .build();
     }
