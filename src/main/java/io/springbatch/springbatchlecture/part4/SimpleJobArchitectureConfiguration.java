0625cc5e311749a3e60ff5e18e060601b9e6d3bb
JunHwan Kim <nefertiri972@gmail.com>
2022-10-29 21:26:43 +0900

    Add step builder configuration
    - StepBuilderFactory
      - StepBuilder 객체 생성
    - StepBuilderHelper
      - 모든 StepBuilder 구현체가 상속받음
      - 내부에 CommonStepProperties 객체를 가지고 있음
      - CommonStepProperties 내부에 JobRepository 참조
    - StepBuilder
      - API 설정에 따라서 TaskletStepBuilder, SimpleStepBuilder, PartitionStepBuilder, JobStepBuilder, FlowStepBuilder 객체 생성
    - JobRepository는 빌더 클래스를 통해 Step객체에 전달되어 메타데이터를 기록

--- src/main/java/io/springbatch/springbatchlecture/part4/SimpleJobArchitectureConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/part4/SimpleJobArchitectureConfiguration.java
@@ -8,10 +8,9 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class SimpleJobArchitectureConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
