2a861995cbcd1e7431b39d3bb63cc3d4a1433f3d
JunHwan Kim <nefertiri972@gmail.com>
2022-11-01 22:53:48 +0900

    Add tasklet step architecture configuration
    - Job -> Step -> RepeatTemplate -> Tasklet -> Business Logic -> Repeat Status
      - Tasklet 안에서 예외가 발생하면 반복문(RepeatTemplate) 및 Step 종료
      - Business Logic 종료시 Repeat Status가 FINISHED면 반복문 종료 및 Step 종료
      - Repeat Status가 CONTINUABLE이면 RepeatTemplate에서 다시 Tasklet 반복
    
    - SimpleJob 객체가 StepHandler를 통해 Step을 실행
      - StepExecution 객체를 생성하고, ExecutionContext 객체를 생성하여 StepExecution 객체에 저장
      - Step 실행 전 Listener 호출(AbstractStep) 후 Step 실행
      - TaskletStep에서 RepeatTemplate 객체를 통해 반복
        - TransactionTemplate 객체를 통해 트랜잭션 처리
        - Tasklet 실행
      - Step 실행 후 Listener 호출

--- src/main/java/io/springbatch/springbatchlecture/part5/LimitAllowConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/part5/LimitAllowConfiguration.java
@@ -10,10 +10,9 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
-@Configuration
+//@Configuration
 public class LimitAllowConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
