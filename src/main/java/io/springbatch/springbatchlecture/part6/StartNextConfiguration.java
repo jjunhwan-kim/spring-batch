bfa1a66d7afefe4ec979e92ca6733ca28c631a73
JunHwan Kim <nefertiri972@gmail.com>
2022-11-04 22:34:09 +0900

    Add start next configuration
    - JobBuilderFactory.start(flow)
      - 처음 실행 할 Flow 설정, JobFlowBuilder가 반환
      - 여기에 Step 이 인자로 오게 되면 SimpleJobBuilder가 반환

--- src/main/java/io/springbatch/springbatchlecture/part6/StartNextConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/part6/StartNextConfiguration.java
@@ -6,14 +6,18 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 
 @RequiredArgsConstructor
-//@Configuration
-public class FlowJobConfiguration {
+@Configuration
+public class StartNextConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -21,14 +25,32 @@ public class FlowJobConfiguration {
     @Bean
     public Job batchJob() {
         return jobBuilderFactory.get("batchJob")
-                .start(step1())
-                .on("COMPLETED") // Step의 실행 결과로 돌려받는 종료상태(ExitStatus)를 캐치하여 매칭하는 패턴
-                .to(step3())
-                .from(step1())
-                .on("FAILED")
-                .to(step2())
+                .start(flowA())
+                .next(step3())
+                .next(flowB())
+                .next(step6())
                 .end()
                 .build();
+    }
+
+    @Bean
+    public Flow flowA() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowA");
+        flowBuilder.start(step1())
+                .next(step2())
+                .end();
+
+        return flowBuilder.build();
+    }
+
+    @Bean
+    public Flow flowB() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flowB");
+        flowBuilder.start(step4())
+                .next(step5())
+                .end();
+
+        return flowBuilder.build();
     }
 
     @Bean
@@ -39,7 +61,6 @@ public class FlowJobConfiguration {
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
                         System.out.println("step1 has executed");
                         return RepeatStatus.FINISHED;
-                        //throw new RuntimeException("step1 was failed");
                     }
                 })
                 .build();
@@ -70,4 +91,45 @@ public class FlowJobConfiguration {
                 })
                 .build();
     }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step4 has executed");
+                        return RepeatStatus.FINISHED;
+                        //throw new RuntimeException("step4 was failed");
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step5")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step5 has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step6() {
+        return stepBuilderFactory.get("step6")
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("step6 has executed");
+                        return RepeatStatus.FINISHED;
+                    }
+                })
+                .build();
+    }
+
 }
