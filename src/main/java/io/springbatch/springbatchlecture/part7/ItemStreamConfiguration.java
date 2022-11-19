afc0a3e9333b6f234b04edb2b4afe68738d6922c
JunHwan Kim <nefertiri972@gmail.com>
2022-11-19 23:34:43 +0900

    Add item stream configuration
    - ItemStream
    - ItemReader와 ItemWriter 처리 과정 중 상태를 저장하고 오류가 발생하면 해당 상태를 참조하여 실패한 곳에서 재시작 하도록 지원
    - 리소스를 열고 닫아야하며 입출력 장치 초기화 등의 작업을 해야하는 경우
    - ExecutionContext를 매개변수로 받아서 상태 정보를 업데이트함
    - ItemReader, ItemWriter는 ItemStream 인터페이스를 구현해야 함
    - ItemStream.open 최초 1회 호출 -> ItemReader -> ItemStream.update 청크 단위로 호출
    - ItemStream.open 최초 1회 호출 -> ItemWriter -> ItemStream.update 청크 단위로 호출
    - AbstractStep의 execute 메서드에서 open 메서드를 호출하여 ItemStreamReader, ItemStreamWriter 의 open 메서드를 먼저 호출함

--- src/main/java/io/springbatch/springbatchlecture/part7/ItemStreamConfiguration.java
+++ src/main/java/io/springbatch/springbatchlecture/part7/ItemStreamConfiguration.java
@@ -5,18 +5,19 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RequiredArgsConstructor
-//@Configuration
-public class ItemReaderProcessorWriterConfiguration {
+@Configuration
+public class ItemStreamConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
@@ -32,30 +33,27 @@ public class ItemReaderProcessorWriterConfiguration {
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .<Customer, Customer>chunk(3)
+                .<String, String>chunk(5)
                 .reader(itemReader())
-                .processor(itemProcessor())
                 .writer(itemWriter())
                 .build();
     }
 
     @Bean
-    public ItemReader<? extends Customer> itemReader() {
-        return new CustomItemReader(Arrays.asList(
-                new Customer("user1"),
-                new Customer("user2"),
-                new Customer("user3")));
+    public ItemReader<String> itemReader() {
+        List<String> items = new ArrayList<>(10);
+
+        for (int i = 1; i <= 10; i++) {
+            items.add(String.valueOf(i));
+        }
+
+        return new CustomItemStreamReader(items);
     }
 
-    @Bean
-    public ItemProcessor<? super Customer, ? extends Customer> itemProcessor() {
-        return new CustomItemProcessor();
+    private ItemWriter<? super String> itemWriter() {
+        return new CustomItemStreamWriter();
     }
 
-    @Bean
-    public ItemWriter<? super Customer> itemWriter() {
-        return new CustomItemWriter();
-    }
 
     @Bean
     public Step step2() {
